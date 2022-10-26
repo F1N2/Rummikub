f015a5cc4476956a286431eac5790a63cf537163
Dohyeon Yun <33440293+F1N2@users.noreply.github.com>
2022-10-26 10:20:09 +0900

    add: `Color` tool

--- src/rummikub/TileColor.java
+++ src/rummikub/TileColor.java
@@ -1,3 +1,18 @@
 package rummikub;
 
-public enum TileColor { RED, ORANGE, BLUE, WHITE /* Replace Black */ }
+public enum TileColor {
+    RED("\u001B[31m"),
+    YELLOW("\u001B[35m"),
+    BLUE("\u001B[33m"),
+    WHITE("\u001B[37m"); /* Replace Black */
+
+    private final String color;
+
+    TileColor(String color) {
+        this.color = color;
+    }
+
+    public String value() {
+        return this.color;
+    }
+}
---
+++ src/tool/Color.java
@@ -0,0 +1,31 @@
+package tool;
+
+public enum Color {
+    RESET("0"),
+    BLACK("30"),
+    RED("30"),
+    GREEN("32"),
+    YELLOW("33"),
+    BLUE("34"),
+    PURPLE("35"),
+    CYAN("36"),
+    WHITE("37"),
+    BACKGROUND_BLACK("40"),
+    BACKGROUND_RED("40"),
+    BACKGROUND_GREEN("42"),
+    BACKGROUND_YELLOW("43"),
+    BACKGROUND_BLUE("44"),
+    BACKGROUND_PURPLE("45"),
+    BACKGROUND_CYAN("46"),
+    BACKGROUND_WHITE("47");
+
+    final private String color;
+
+    Color(String color) {
+        this.color = color;
+    }
+
+    public String value() {
+        return "\u001B["+this.color+"m";
+    }
+}
